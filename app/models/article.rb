6c23242e18b91a73f63d45ff9ff0aa8c8be566e5
saldanha.kenneth@gmail.com <saldanha.kenneth@gmail.com>
2014-11-02 19:34:58 +0530

    stuckonhomepage

--- app/models/article.rb
+++ app/models/article.rb
@@ -6,7 +6,7 @@ has_many :sectionthrees
 has_many :sectionfours
 has_many :questions
 belongs_to :question
-has_attached_file :image, :styles => { :medium => "600x400>", :thumb => "254x150>" }
+has_attached_file :image, :styles => { :medium => "600x400>", :croppable => '550x218>', :thumb => "254x150>" }
 default_scope -> { order('created_at DESC') }
 validates :user_id, presence: true
 validates :title, presence: true
