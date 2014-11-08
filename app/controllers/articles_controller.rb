f31546388933fc5a931eb6e0d7f1e6f008d5d456
saldanha.kenneth@gmail.com <saldanha.kenneth@gmail.com>
2014-11-08 17:18:36 +0530

    articleindex

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -47,7 +47,7 @@ class ArticlesController < ApplicationController
   end
 
   def tagged
-  @questions = Question.tagged(params[:tag])
+  @questions = Question.tagged_with(params[:tag])
   @user = current_user
   if params[:tag].present? 
     @articles = Article.tagged_with(params[:tag])
