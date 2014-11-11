58175ee5452d35f98bca3db96902f51c2ae6e304
saldanha.kenneth@gmail.com <saldanha.kenneth@gmail.com>
2014-11-11 13:47:19 +0530

    glyphiconissue

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,6 +28,7 @@ ConsumerTadka::Application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
+  config.assets.precompile += %w( .svg .eot .woff .ttf )
 
   # Generate digests for assets URLs.
   config.assets.digest = true
